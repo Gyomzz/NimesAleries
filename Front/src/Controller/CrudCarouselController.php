2d1cb57df249b2601674d01f4a931202493342a5
gyommz <millet.g@hotmail.com>
2021-11-16 23:45:50 +0100

    close #1

---
+++ Front/src/Controller/CrudCarouselController.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Carousel;
+use App\Form\CarouselType;
+use App\Repository\CarouselRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+/**
+ * @Route("/crud/carousel")
+ */
+class CrudCarouselController extends AbstractController
+{
+    /**
+     * @Route("/", name="crud_carousel_index", methods={"GET"})
+     */
+    public function index(CarouselRepository $carouselRepository): Response
+    {
+        return $this->render('crud_carousel/index.html.twig', [
+            'carousels' => $carouselRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="crud_carousel_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $carousel = new Carousel();
+        $form = $this->createForm(CarouselType::class, $carousel);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($carousel);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('crud_carousel_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('crud_carousel/new.html.twig', [
+            'carousel' => $carousel,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="crud_carousel_show", methods={"GET"})
+     */
+    public function show(Carousel $carousel): Response
+    {
+        return $this->render('crud_carousel/show.html.twig', [
+            'carousel' => $carousel,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="crud_carousel_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Carousel $carousel): Response
+    {
+        $form = $this->createForm(CarouselType::class, $carousel);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('crud_carousel_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('crud_carousel/edit.html.twig', [
+            'carousel' => $carousel,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="crud_carousel_delete", methods={"POST"})
+     */
+    public function delete(Request $request, Carousel $carousel): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$carousel->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($carousel);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('crud_carousel_index', [], Response::HTTP_SEE_OTHER);
+    }
+    public function getCarouselOrder(){
+        $carousel = $this->getDoctrine()
+            ->getRepository(Carousel::class)
+            ->findCarouselByOrders();
+        $session = $this->requestStack->getSession();
+        $session = $this->get('session');        
+        $session->set('carousel', $carousel);
+
+        if(!$carousel){
+            throw $this->createNotFoundException(
+                'No category found'
+            );
+        }
+        // return $carousel;
+    }
+    public function __construct(RequestStack $requestStack)
+    {
+        $this->requestStack = $requestStack;
+    }
+}
---
+++ Front/src/Entity/Carousel.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CarouselRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CarouselRepository::class)
+ */
+class Carousel
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $Title;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $image;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $ordering;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->Title;
+    }
+
+    public function setTitle(string $Title): self
+    {
+        $this->Title = $Title;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getOrdering(): ?int
+    {
+        return $this->ordering;
+    }
+
+    public function setOrdering(?int $ordering): self
+    {
+        $this->ordering = $ordering;
+
+        return $this;
+    }
+}
