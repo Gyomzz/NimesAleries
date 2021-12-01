3d83b2bf10005f2bc55902f1d9fa2e540b55db52
gyommz <millet.g@hotmail.com>
2021-12-01 21:31:31 +0100

    add missing repo

--- Front/src/Entity/Orders.php
+++ Front/src/Entity/Orders.php
@@ -2,6 +2,7 @@
 
 namespace App\Entity;
 
+use App\Repository\OrdersRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
