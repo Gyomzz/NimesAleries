0ab020e5989298b1bdcc572b664ddb46702dd9c0
gyommz <millet.g@hotmail.com>
2021-12-07 12:26:52 +0100

    crud perso carousel

--- Front/src/Form/CarouselType.php
+++ Front/src/Form/CarouselType.php
@@ -5,6 +5,10 @@ namespace App\Form;
 use App\Entity\Carousel;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\File;
@@ -14,9 +18,11 @@ class CarouselType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('Title')
+            ->add('Title', TextType::class, [
+                'label' => false,
+            ])
             ->add('picture', FileType::class, [
-                'label' => 'Image (Jpeg, Png)',
+                'label' => false,
 
                 // unmapped means that this field is not associated to any entity property
                 'mapped' => false,
@@ -38,9 +44,19 @@ class CarouselType extends AbstractType
                     ])
                 ],
             ])
-            ->add('ordering')
-            ->add('description')
-            ->add('teaser')
+            ->add('ordering', IntegerType::class, [
+                'label' => false,
+            ])
+            ->add('description', TextareaType::class, [
+                'attr' => ['rows' => 6],
+                'label' => false,
+            ])
+            ->add('teaser', TextType::class, [
+                'label' => false,
+            ])
+            ->add('save', SubmitType::class, [
+                'label' => 'Valider'
+            ])
         ;
     }
 
