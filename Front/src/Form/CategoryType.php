77d1bb260bec85cee8276368f4fb8eded97dab98
gyommz <millet.g@hotmail.com>
2021-12-07 15:04:08 +0100

    crud perso category

--- Front/src/Form/CategoryType.php
+++ Front/src/Form/CategoryType.php
@@ -5,6 +5,9 @@ namespace App\Form;
 use App\Entity\Category;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,15 +16,22 @@ class CategoryType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('ordering')
+            ->add('name', TextType::class, [
+                'label' => false,
+            ])
+            ->add('ordering', IntegerType::class, [
+                'label' => false,
+            ])
             ->add('id_parent', EntityType::class , [
-                'label' => 'Categorie parente',
+                'label' => false,
                 'class' => Category::class,
                 'choice_label' => function ($category) {
                     return $category->getName();
                 },
                 'required' => false,
+            ])
+            ->add('save', SubmitType::class, [
+                'label' => 'Valider'
             ])
         ;
     }
