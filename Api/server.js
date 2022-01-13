c3e29d307b1861d2309bb4fefb623ccbb41b3b6a
Gyommz <millet.g@hotmail.com>
2022-01-13 17:48:24 +0100

    server: 404 error

--- Api/server.js
+++ Api/server.js
@@ -42,13 +42,16 @@ app.all('/', function(req,res){
   res.redirect('/request');
 });
 
+app.use(function(req, res, next) {
+  res.status(404).send('Sorry cant find that!');
+});
+
+app.use(function(err, req, res, next) {
+  res.status(500).send('Something broke!');
+});
 
 app.listen(process.env.PORT || 6060, () => {
     console.log(`Server is running on port : ${PORT}`);
 })
 
 
-app.use(function(err, req, res, next) {
-  console.error(err.stack);
-  res.status(500).send('Something broke!');
-});
