12558186b59fe5cb32888cf3217082c4ef0648bb
Gyommz <millet.g@hotmail.com>
2022-02-05 22:49:20 +0100

    get token form

--- Api/server.js
+++ Api/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
-
+const morgan = require('morgan')
+const jwt = require('jsonwebtoken')
 const path = require('path');
 const PORT = process.env.SERVER_PORT;
 const express = require('express');
@@ -17,7 +18,38 @@ const config = {
 
 global.appRoot = path.resolve(__dirname);
 
-app.use(cors());
+app.use(cors())           
+app.use(morgan('tiny'))                    
+app.use(express.json())                 
+app.use(express.urlencoded({ extended: true }))
+
+// List of Users
+const users = [
+    { id: 1, username: 'admin', password: '123' }
+]
+
+/* Connexion Form */
+app.post('/login', (req, res) => {
+    // If no data
+    if (!req.body.username || !req.body.password) {
+        return res.status(400).json({ message: 'Error. Please enter the correct username and password' })
+    }
+
+    // Checking
+    const user = users.find(u => u.username === req.body.username && u.password === req.body.password)
+
+    // Wrong Data
+    if (!user) {
+        return res.status(400).json({ message: 'Error. Wrong login or password' })
+    }
+
+    const token = jwt.sign({
+        id: user.id,
+        username: user.username
+    }, SECRET, { expiresIn: '3 hours' })
+
+    return res.json({ access_token: token })
+})
 
 config.connection.connect();
 
@@ -53,5 +85,3 @@ app.use(function(err, req, res, next) {
 app.listen(process.env.PORT || 6060, () => {
     console.log(`Server is running on port : ${PORT}`);
 })
-
-
